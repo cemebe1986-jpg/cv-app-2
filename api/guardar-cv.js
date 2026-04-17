2d0cbc3ff0a828d237a0f021c230669b503c73cb
Cesar M <cemebe1986@gmail.com>
2026-04-17 07:40:10 -0500

    Mercado Pago 7

--- api/guardar-cv.js
+++ api/guardar-cv.js
@@ -7,8 +7,8 @@ module.exports = async (req, res) => {
   
   try {
     const redis = new Redis({
-      url: process.env.UPSTASH_REDIS_REST_URL || process.env.KV_REST_API_URL,
-      token: process.env.UPSTASH_REDIS_REST_TOKEN || process.env.KV_REST_API_TOKEN,
+      url: process.env.KV_REST_API_URL,
+      token: process.env.KV_REST_API_TOKEN,
     });
 
     // ID aleatorio para la descarga (compatible con flujo existente)
